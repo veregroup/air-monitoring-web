423215abb23c3e10c2a4c3492f14643f74166c18
JDOGG <verejohndavid@gmail.com>
2025-04-30 22:28:01 +0800

    Set login page as the homepage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import History from './pages/History';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/air-monitoring-web">
       <nav style={{ padding: 10 }}>
         <Link to="/" style={{ margin: 10 }}>Home</Link>
         <Link to="/map" style={{ margin: 10 }}>Map</Link>
         <Link to="/history" style={{ margin: 10 }}>History</Link>
       </nav>
       <Routes>
-      <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Home />} />
+        {/* Default path is now the LoginPage */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} /> {/* Optional fallback for "/login" */}
+        <Route path="/home" element={<Home />} />
         <Route path="/map" element={<MapView />} />
         <Route path="/history" element={<History />} />
         {/* Fallback route for unmatched paths */}
